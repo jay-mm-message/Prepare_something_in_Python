274bd18a1719c5c42a26e0c171eafde844a229d8
jay <jay.mm.message@gmail.com>
2023-05-01 00:13:07 +0800

    Use the Python datetime library to determine the day of the week for today

---
+++ Chapter_1/test_today.py
@@ -0,0 +1,22 @@
+
+from datetime import datetime
+
+today = datetime.today().day
+
+#print("Today is ", today)
+if today == 6:
+    print("Party!!!")
+elif today == 7:
+    print("Recover")
+elif today == 1:
+    print("Today is Monday Work Work Work")
+elif today == 2:
+    print("Today is Tuesday Work Work Work")
+elif today == 3:
+  print("Today is Wednesday Work Work Work")
+elif today == 4:
+  print("Today is Thursday Work Work Work")
+elif today == 5:
+  print("Today is Friday Work Work Work")
+else:
+  print("none")
